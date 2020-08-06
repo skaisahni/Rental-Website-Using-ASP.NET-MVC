d91b55ee30e3a115a8931e47ed1a083cd21b9cf5
DRISHTI SINGH <30665104+naina013@users.noreply.github.com>
2020-08-06 06:22:19 +0100

    making new routing

--- AIT_Rental/Controllers/MoviesController.cs
+++ AIT_Rental/Controllers/MoviesController.cs
@@ -9,11 +9,31 @@ namespace AIT_Rental.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "dangal" };
-            return View(movie);
+
+            //return View(movie);
+            return RedirectToAction("Index","Home", new { page =1 , sortBy ="name"});
         }
+        public ActionResult Edit(int MovieId)
+        {
+            return Content("id=" + MovieId);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy) 
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        
+        }
+        public ActionResult ByReleaseDate(int year, int month) {
+            return Content(year+"/"+month);
+         }
+
     }
 }
